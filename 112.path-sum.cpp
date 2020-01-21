26d55213aa5c8a30ba638a8fbf1ca78036c6e566
stucci <sts.ucd@gmail.com>
2020-01-22 00:45:25 +0900

    121.best-time-to-buy-and-sell-stock.cpp

---
+++ 112.path-sum.cpp
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode id=112 lang=cpp
+ *
+ * [112] Path Sum
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int sum) {
+        
+    }
+};
+// @lc code=end
+
--- 121.best-time-to-buy-and-sell-stock.cpp
+++ 121.best-time-to-buy-and-sell-stock.cpp
@@ -10,7 +10,10 @@ using namespace std;
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        
+        int mi=INT_MAX, ma=0;
+        for(int i = 0; i < prices.size(); i++)
+            mi>prices[i] ? mi=prices[i] : ma=max(ma,prices[i]-mi);
+        return ma;
     }
 };
 // @lc code=end
