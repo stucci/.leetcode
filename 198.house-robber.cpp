d4040648accbe695bb9ac35ed96e3464d3d4e182
stucci <sts.ucd@gmail.com>
2020-01-24 01:42:51 +0900

    198.house-robber.cpp

--- 198.house-robber.cpp
+++ 198.house-robber.cpp
@@ -10,24 +10,16 @@ using namespace std;
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        vector<int> dp(1000);
+        vector<int> dp(100, -1);
         return dfs(nums, dp, 0);
-        // if(nums.size()<=0) return 0;
-        // if(nums.size()<=1) return nums[0];
-        // if(nums.size()<=2) return max(nums[0], nums[1]);
-        // if(nums.size()<=3) return max(nums[0]+nums[2], nums[1]);
-        // vector<int> first(nums.begin()+2, nums.end());
-        // vector<int> second(nums.begin()+3, nums.end());
-        // return max(nums[0]+rob(first), nums[1]+rob(second));
     }
-    int dfs(vector<int>& nums, vector<int>& dp, int begin){
-        if(nums.size()<=begin) return 0;
-        if(nums.size()<=begin+1) return nums[begin];
-        if(nums.size()<=begin+2) return max(nums[begin], nums[begin+1]);
-        if(nums.size()<=begin+3) return max(nums[begin]+nums[begin+2], nums[begin+1]);
-        // vector<int> first(nums.begin()+2, nums.end());
-        // vector<int> second(nums.begin()+3, nums.end());
-        return max(nums[begin]+dfs(nums, dp, 2), nums[begin+1]+dfs(nums, dp, 3));
+    int dfs(vector<int>& nums, vector<int>& dp, int i){
+        if(nums.size()<=i) return 0;
+        if(nums.size()<=i+1) return nums[i];
+        if(nums.size()<=i+2) return max(nums[i], nums[i+1]);
+        if(nums.size()<=i+3) return max(nums[i]+nums[i+2], nums[i+1]);
+        if(dp[i]==-1) dp[i] = max(nums[i]+dfs(nums, dp, i+2), nums[i+1]+dfs(nums, dp, i+3));
+        return dp[i];
     }
 };
 // @lc code=end
