d781aa2e78a9b1cd255767fd8bc5c8f0deee7e54
stucci <sts.ucd@gmail.com>
2020-01-26 14:59:03 +0900

    387.first-unique-character-in-a-string.cpp use hash map

--- 387.first-unique-character-in-a-string.cpp
+++ 387.first-unique-character-in-a-string.cpp
@@ -10,15 +10,10 @@ using namespace std;
 class Solution {
 public:
     int firstUniqChar(string s) {
-        unordered_multiset<char> ums(s.begin(), s.end());
-        int ret=-1;
-        for (size_t i = 0; i < s.length(); i++) {
-            if(ums.count(s[i])==1) {
-                ret=i;
-                break;
-            }
-        }
-        return ret;
+        unordered_map<char, int> um;
+        for (size_t i = 0; i < s.length(); i++) um[s[i]]++;
+        for (size_t i = 0; i < s.length(); i++) if(um[s[i]]==1) return i;
+        return -1;
     }
 };
 // @lc code=end
