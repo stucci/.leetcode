e4be88490547f182b3f9b1b26177c80ef6408290
stucci <sts.ucd@gmail.com>
2020-01-27 22:53:06 +0900

    shorter

--- 7.reverse-integer.py
+++ 7.reverse-integer.py
@@ -7,21 +7,12 @@
 # @lc code=start
 class Solution:
     def reverse(self, x: int) -> int:
-        if x==0:
+        sign=(x>0)-(x<0)
+        s=str(x*sign)
+        r=s[::-1]
+        ret=int(r)
+        if ret>2**31-1:
             return 0
-        nega=False
-        if x<0:
-            nega=True
-            x*=(-1)
-        s=str(x)
-        rs=s[::-1]
-        rs=re.sub(r'^0+',"",rs)
-        ret=int(rs)
-        if nega:
-            ret*=(-1)
-        int_max=pow(2,31)
-        if ret>int_max-1 or ret<(-1)*int_max:
-            return 0
-        return ret
+        return sign*ret
 # @lc code=end
 
