bbd8c04a9b99cd510ac1706577e31ba7325b6c59
stucci <sts.ucd@gmail.com>
2020-02-11 11:30:00 +0900

    15 init

--- 15.3-sum.cpp
+++ 15.3-sum.cpp
@@ -11,19 +11,41 @@ class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         set<vector<int>> s;
+        sort(nums.begin(), nums.end());
+        int i=0,j=0;
+        while(i<nums.size()&&j<nums.size()){
+            if(nums[i]==nums[j]){
+                j++;
+            }else{
+                if(j-i>=3){
+                    nums.erase(nums.begin()+i+3,nums.begin()+j);
+                }
+                i=j;
+                j++;
+            }
+            if(j==nums.size()&&j-1-i>=3){
+                nums.erase(nums.begin()+i+3,nums.begin()+j-1);
+            }
+        }
         for (int i = 0; i < nums.size(); i++) {
             for (int j = i+1; j < nums.size(); j++) {
-                for (int k = j+1; k < nums.size(); k++) {
-                    if(nums[i]+nums[j]+nums[k]==0) {
-                        vector<int> tmp({nums[i],nums[j],nums[k]});
-                        sort(tmp.begin(), tmp.end());
-                        s.insert(tmp);
+                int lo=j+1, hi=nums.size()-1;
+                while(hi-lo>=0){
+                    int mid=(lo+hi)/2;
+                    int sum=nums[i]+nums[j]+nums[mid];
+                    if(sum==0){
+                        s.insert({nums[i],nums[j],nums[mid]});
+                        break;
+                    }else if(sum>0){
+                        hi=mid-1;
+                    }else{
+                        lo=mid+1;
                     }
                 }
             }
         }
-        vector<vector<int>> v(s.begin(),s.end());
-        return v;
+        vector<vector<int>> ret(s.begin(),s.end());
+        return ret;
     }
 };
 // @lc code=end
