ffbc09cd92e533da28fcf1673c79e5593f9ffbc8
stucci <sts.ucd@gmail.com>
2020-02-11 12:00:01 +0900

    15 fix one value and seach left two value by two pointer

--- 15.3-sum.cpp
+++ 15.3-sum.cpp
@@ -10,41 +10,25 @@ using namespace std;
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        set<vector<int>> s;
+        vector<vector<int>> ret;
         sort(nums.begin(), nums.end());
-        int i=0,j=0;
-        while(i<nums.size()&&j<nums.size()){
-            if(nums[i]==nums[j]){
-                j++;
-            }else{
-                if(j-i>=3){
-                    nums.erase(nums.begin()+i+3,nums.begin()+j);
-                }
-                i=j;
-                j++;
-            }
-            if(j==nums.size()&&j-1-i>=3){
-                nums.erase(nums.begin()+i+3,nums.begin()+j-1);
-            }
-        }
         for (int i = 0; i < nums.size(); i++) {
-            for (int j = i+1; j < nums.size(); j++) {
-                int lo=j+1, hi=nums.size()-1;
-                while(hi-lo>=0){
-                    int mid=(lo+hi)/2;
-                    int sum=nums[i]+nums[j]+nums[mid];
-                    if(sum==0){
-                        s.insert({nums[i],nums[j],nums[mid]});
-                        break;
-                    }else if(sum>0){
-                        hi=mid-1;
-                    }else{
-                        lo=mid+1;
-                    }
+            if(i>0&&nums[i]==nums[i-1]) continue;
+            int lo=i+1, hi=nums.size()-1;
+            while(hi-lo>0){
+                int sum=nums[i]+nums[lo]+nums[hi];
+                if(sum==0){
+                    ret.push_back({nums[i],nums[lo],nums[hi]});
+                    while(lo<nums.size()-1 && nums[lo]==nums[lo+1]) lo++;
+                    while(hi>0 && nums[hi]==nums[hi-1]) hi--;
+                    lo++,hi--;
+                }else if(sum>0){
+                    hi--;
+                }else{
+                    lo++;
                 }
             }
         }
-        vector<vector<int>> ret(s.begin(),s.end());
         return ret;
     }
 };
