8eecbdffcd234fa6248fdf3920d5a1f1959e1e7a
stucci <sts.ucd@gmail.com>
2020-02-13 01:57:42 +0900

    70 dp

--- 70.climbing-stairs.cpp
+++ 70.climbing-stairs.cpp
@@ -10,21 +10,13 @@ using namespace std;
 class Solution {
 public:
     int climbStairs(int n) {
-        long long s=0;
-        for (size_t i = 0; i <= n/2; i++) {
-            s+=nCr(n-i,i);
+        vector<int> dp(n);
+        dp[0]=1;
+        if(n>1) dp[1]=2;
+        for (size_t i = 2; i < n; i++) {
+            dp[i]=dp[i-1]+dp[i-2];
         }
-        return s;
-    }
-    long long nCr(const long long n, const long long k) {
-        if (n < 0) return 0;
-        if (k < 0) return 0;
-        long long ret = 1;
-        for (long long i = 0; i < k; i++) {
-            ret *= (long long)n - (long long)i;
-            ret /= (long long)(i+1);
-        }
-        return ret;
+        return dp[n-1];
     }
 };
 // @lc code=end
