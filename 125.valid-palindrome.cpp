0b444e34a6cd34665066aa77bd73ef70a15b1dc0
stucci <sts.ucd@gmail.com>
2020-02-16 04:12:49 +0900

    125 better

--- 125.valid-palindrome.cpp
+++ 125.valid-palindrome.cpp
@@ -10,9 +10,17 @@ using namespace std;
 class Solution {
 public:
     bool isPalindrome(string s) {
-        transform(s.begin(),s.end(),s.begin(),::tolower);
-        string t=regex_replace(s,regex("[^a-z0-9]"),"");
-        return (t==string(t.rbegin(),t.rend()));
+        // // initial subumission
+        // transform(s.begin(),s.end(),s.begin(),::tolower);
+        // string t=regex_replace(s,regex("[^a-z0-9]"),"");
+        // return (t==string(t.rbegin(),t.rend()));
+
+        for (int i = 0, j = s.size()-1; i < j; i++,j--) {
+            while(!isalnum(s[i]) && i<j) i++;
+            while(!isalnum(s[j]) && i<j) j--;
+            if(tolower(s[i])!=tolower(s[j])) return false;
+        }
+        return true;
     }
 };
 // @lc code=end
