850fa7faab1cd9c6e7a311fc999077565c14e375
stucci <sts.ucd@gmail.com>
2020-02-16 15:10:46 +0900

    169 HashMap improved

--- 169.majority-element.rs
+++ 169.majority-element.rs
@@ -10,17 +10,15 @@ use std::cmp::max;
 impl Solution {
     pub fn majority_element(nums: Vec<i32>) -> i32 {
         let mut m: HashMap<i32,i32> = HashMap::new();
-        let mut max_key: i32 = 0;
-        let mut max_val: i32 = 0;
+        let len = nums.len() as i32;
         for e in nums {
             let cnt = m.entry(e).or_insert(0);
             *cnt += 1;
-            if(max_val<*cnt){
-                max_val=*cnt;
-                max_key=e;
+            if(*cnt>len/2){
+                return e
             }
         }
-        max_key
+        0
     }
 }
 // @lc code=end
