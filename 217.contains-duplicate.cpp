e95badd84b9672ed2e035fdcf36a1ab08bfc91f1
stucci <sts.ucd@gmail.com>
2020-02-24 13:12:21 +0900

    217 set

---
+++ 217.contains-duplicate.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+/*
+ * @lc app=leetcode id=217 lang=cpp
+ *
+ * [217] Contains Duplicate
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> s(nums.begin(), nums.end());
+        return s.size()!=nums.size();
+    }
+};
+// @lc code=end
+
