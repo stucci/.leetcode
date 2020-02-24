97c7120d6c8c4ece3bfd0c618da1a70ed6985c49
stucci <sts.ucd@gmail.com>
2020-02-24 13:03:27 +0900

    268 gauss formula

--- 268.missing-number.cpp
+++ 268.missing-number.cpp
@@ -10,11 +10,17 @@ using namespace std;
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
+        int n=nums.size();
+        int expected_sum=n*(n+1)/2;
+        int sum=accumulate(nums.begin(), nums.end(),0);
+        return expected_sum-sum;
+    }
+    int missingNumberXOR(vector<int>& nums) {
         int missing=nums.size();
         for (long long i = 0; i < nums.size(); i++) {
             missing ^= i^nums[i];
         }
-        return
+        return missing;
     }
     int missingNumberMap(vector<int>& nums) {
         unordered_map<int,bool> um;
