90e5b94073faa73eb9dcfba63600bfad8920a437
stucci <sts.ucd@gmail.com>
2020-02-24 13:17:43 +0900

    242 sort

---
+++ 242.valid-anagram.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+/*
+ * @lc app=leetcode id=242 lang=cpp
+ *
+ * [242] Valid Anagram
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        return s==t;
+    }
+};
+// @lc code=end
+
