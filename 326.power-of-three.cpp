36fb05f47b7ae64218049c07265e1f1bb81d3b99
stucci <sts.ucd@gmail.com>
2020-02-24 12:03:00 +0900

    326 init

---
+++ 326.power-of-three.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+/*
+ * @lc app=leetcode id=326 lang=cpp
+ *
+ * [326] Power of Three
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if(n==0) return false;
+        if(n==1) return true;
+        while(n%3==0){
+            n/=3;
+            if(n==1) return true;
+        }
+        return false;
+    }
+};
+// @lc code=end
+
