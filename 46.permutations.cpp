9343f8cfb6d651755204285b7b2853444c865015
stucci <sts.ucd@gmail.com>
2020-02-27 01:30:55 +0900

    46 backtracking

--- 46.permutations.cpp
+++ 46.permutations.cpp
@@ -11,6 +11,21 @@ class Solution {
 public:
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> v;
+        permuteBacktrack(nums, 0, v);
+        return v;
+    }
+    void permuteBacktrack(vector<int> nums, int i, vector<vector<int>> &v){
+        if(i==nums.size()){
+            v.push_back(nums);
+        }else{
+            for (long long j = i; j < nums.size(); j++) {
+                swap(nums[i], nums[j]);
+                permuteBacktrack(nums,i+1,v);
+            }
+        }
+    }
+    vector<vector<int>> permuteStd(vector<int>& nums) {
+        vector<vector<int>> v;
         sort(nums.begin(), nums.end());
         v.push_back(nums);
         while(next_permutation(nums.begin(), nums.end())){
