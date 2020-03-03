8f288c027e9ef5ea077c24739080b67106c36a21
stucci <sts.ucd@gmail.com>
2020-03-03 23:11:39 +0900

    78 init

---
+++ 78.subsets.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+/*
+ * @lc app=leetcode id=78 lang=cpp
+ *
+ * [78] Subsets
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> ret;
+        for (int bit = 0; bit < (1<<n); bit++) {
+            vector<int> v;
+            for (int i = 0; i < n; i++) {
+                if(bit & (1<<i)) {
+                    v.push_back(nums[i]);
+                }
+            }
+            ret.push_back(v);
+        }
+        return ret;
+    }
+};
+// @lc code=end
+
