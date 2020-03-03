a55eb273815b9712723381a96f4b814c4df1a227
stucci <sts.ucd@gmail.com>
2020-03-03 23:15:13 +0900

    78 golf

--- 78.subsets.cpp
+++ 78.subsets.cpp
@@ -11,6 +11,16 @@ class Solution {
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
         int n = nums.size();
+        vector<vector<int>> ret(1<<n);
+        for (int bit = 0; bit < (1<<n); bit++) {
+            for (int i = 0; i < n; i++) {
+                if(bit & (1<<i)) ret[bit].push_back(nums[i]);
+            }
+        }
+        return ret;
+    }
+    vector<vector<int>> subsetsInit(vector<int>& nums) {
+        int n = nums.size();
         vector<vector<int>> ret;
         for (int bit = 0; bit < (1<<n); bit++) {
             vector<int> v;
