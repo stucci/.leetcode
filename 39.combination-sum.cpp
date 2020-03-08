e27a60aa1f7c84cd88c0a27be90d9da8c1607413
stucci <sts.ucd@gmail.com>
2020-03-09 00:32:58 +0900

    39 faster

--- 39.combination-sum.cpp
+++ 39.combination-sum.cpp
@@ -12,27 +12,18 @@ public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<vector<int>> v;
         vector<int> t;
+        sort(candidates.begin(), candidates.end());
         helper(candidates, target, 0, v, t);
         return v;
     }
     void helper(vector<int>& candidates, int target, int index,  vector<vector<int>>& v, vector<int> t){
-        for (long long i = 0; i < t.size(); i++) {
-            cout << "t[i]: " << t[i] << " ";
-        }
-        cout << "target: " << target << endl;
         if(target==0){
             v.push_back(t);
             return;
-        }else if(target<0){
-            return;
         }
-        for (long long i = index; i < candidates.size(); i++) {
+        for (long long i = index; i < candidates.size() && target-candidates[i]>=0; i++) {
             t.push_back(candidates[i]);
             helper(candidates, target-candidates[i], i, v, t);
-            for (long long i = 0; i < t.size(); i++) {
-                cout << "t[i]: " << t[i] << " ";
-            }
-            cout << "target befort popback: " << target << endl;
             t.pop_back();
         }
     }
