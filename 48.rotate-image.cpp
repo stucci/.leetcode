b6d8ad0931165651640e605bbfc91092a264bf00
stucci <sts.ucd@gmail.com>
2020-03-11 00:00:25 +0900

    48 improved

--- 48.rotate-image.cpp
+++ 48.rotate-image.cpp
@@ -11,7 +11,6 @@ class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
         for (long long i = 0; i < matrix[0].size()/2; i++) {
-            // rotateRec(matrix, i);
             int beg=i;
             int end=matrix[0].size()-1-beg;
             for (long long i = 0; i < end-beg; i++) {
@@ -19,15 +18,6 @@ public:
                 swap(matrix[beg][i+beg],matrix[end][end-i]);
                 swap(matrix[beg][i+beg],matrix[end-i][beg]);
             }
-        }
-    }
-    void rotateRec(vector<vector<int>>& matrix, int idx) {
-        int beg=idx;
-        int end=matrix[0].size()-1-idx;
-        for (long long i = 0; i < end-beg; i++) {
-            swap(matrix[beg][i+beg],matrix[beg+i][end]);
-            swap(matrix[beg][i+beg],matrix[end][end-i]);
-            swap(matrix[beg][i+beg],matrix[end-i][beg]);
         }
     }
 };
