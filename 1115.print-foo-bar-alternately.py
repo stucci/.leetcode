932bf1623e6d1eda60e0565e25ce60d8efea7f63
stucci <sts.ucd@gmail.com>
2021-04-13 01:00:18 +0900

    1115.print-foo-bar-alternately.py

---
+++ 1115.print-foo-bar-alternately.py
@@ -0,0 +1,35 @@
+#
+# @lc app=leetcode id=1115 lang=python3
+#
+# [1115] Print FooBar Alternately
+#
+
+# @lc code=start
+from threading import Lock
+
+class FooBar:
+    def __init__(self, n):
+        self.n = n
+        self.foo_lock = Lock()
+        self.bar_lock = Lock()
+        self.bar_lock.acquire()
+
+    def foo(self, printFoo: 'Callable[[], None]') -> None:
+        
+        for i in range(self.n):
+            self.foo_lock.acquire()
+            # printFoo() outputs "foo". Do not change or remove this line.
+            printFoo()
+            self.bar_lock.release()
+
+    def bar(self, printBar: 'Callable[[], None]') -> None:
+        
+        for i in range(self.n):
+            
+            self.bar_lock.acquire()
+            # printBar() outputs "bar". Do not change or remove this line.
+            printBar()
+            self.foo_lock.release()
+
+# @lc code=end
+
